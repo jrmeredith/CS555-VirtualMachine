c11dd468a753d1f8f7da43de79740d508036fb0a
Jake Meredith <36305585+jrmeredith@users.noreply.github.com>
2018-07-03 12:18:39 -0500

    Added StackVM member definitions
    Added defintions for all StackVM member functions
    
    * Constructor
    * getType
    * getData
    * fetch
    * decode
    * execute
    * doPrimitive
    * run
    * loadProgram

---
+++ stack-vm.cpp
@@ -0,0 +1,79 @@
+#include "stack-vm.h"
+using namespace std;
+
+/*
+ * Instruction format:
+ * header: 2 bits
+ * data: 30 bits
+ *
+ * header format:
+ * 0 => positive integer
+ * 1 => primitive instruction
+ * 2 => negative integer
+ * 3 => undefined
+ */
+
+// Functions
+StackVM::StackVM() {
+	memory.reserve(1000000);
+}
+
+i32 StackVM::getType(i32 instruction) {
+	i32 type = 0xC0000000;
+	type = (type & instruction) >> 30;
+	return type;
+}
+
+i32 StackVM::getData(i32 instruction) {
+	i32 data = 0x3FFFFFFF;
+	data = data & instruction;
+	return data;
+}
+
+void StackVM::fetch() {
+	pc++;
+}
+
+void StackVM::decode() {
+	typ = getType(memory[pc]);
+	dat = getData(memory[pc]);
+}
+
+void StackVM::execute() {
+	if (typ == 0 || typ == 2) {
+		sp++;
+		memory[sp] = dat;
+	}
+	else {
+		doPrimitive();
+	}
+}
+
+void StackVM::doPrimitive() {
+	switch (dat) {
+	case 0:	// halt
+		cout << "halt" << endl;
+		running = 0;
+		break;
+	case 1:	// add
+		cout << "add " << memory[sp - 1] << " " << memory[sp] << endl;
+		memory[sp - 1] = memory[sp - 1] + memory[sp];
+		break;
+	}
+}
+
+void StackVM::run() {
+	pc -= 1;
+	while (running == 1) {
+		fetch();
+		decode();
+		execute();
+		cout << "tos: " << memory[sp] << endl;
+	}
+}
+
+void StackVM::loadProgram(vector<i32> prog) {
+	for (i32 i = 0; i < prog.size(); i++) {
+		memory[pc + i] = prog[i];
+	}
+}
