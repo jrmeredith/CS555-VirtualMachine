33911947eccf4056d5ad07e55cc8bfd0c69afc99
Jake Meredith <36305585+jrmeredith@users.noreply.github.com>
2018-07-03 11:49:34 -0500

    Renamed a file and updated header
    Renamed vm.cpp to stack-vm.cpp
    Added StackVM class to header along with member variables and
    function declarations.
    
    * Renamed vm.cpp->stack-vm.cpp
    * Added class StackVM
    * Added member variables and functions to StackVM

--- stack-vm.h
+++ stack-vm.h
@@ -4,5 +4,33 @@
 #include <iostream>
 #include <vector>
 
+// type definitions
+typedef int32_t i32;
+
+class StackVM {
+private:
+	// Private variables
+	i32 pc = 100;	// Program counter
+	i32 sp = 0;		// Stack pointer
+	std::vector<i32> memory;
+	i32 typ = 0;
+	i32 dat = 0;
+	i32 running = 1;
+
+	// Private functions
+	i32 getType(i32 instruction);
+	i32 getData(i32 instruction);
+	void fetch();
+	void decode();
+	void execute();
+	void doPrimitive();
+
+public:
+	// Public functions
+	StackVM();
+	void run();
+	void loadProgram(std::vector<i32> prog);
+};
+
 
 #endif // !STACK_VM_H
