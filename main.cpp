24bd9425bc5c6cef1e5fe635da4f21c67962fe70
Jake Meredith <36305585+jrmeredith@users.noreply.github.com>
2018-07-04 09:29:14 -0500

    Added a pause to main.cpp
    Added a pause statement in main.cpp so the console window
    will stay open and wait for user input.

--- main.cpp
+++ main.cpp
@@ -5,7 +5,9 @@ int main() {
 	StackVM vm;
 	vector<i32> prog{3, 4, 0x40000001, 0x40000000 };
 	vm.loadProgram(prog);
+	cout << "Program loaded" << endl;
 	vm.run();
 
+	system("pause");
 	return 0;
 }
