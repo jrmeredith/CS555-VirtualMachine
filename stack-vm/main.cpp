bb1f73848fdbe2b97c92c7348e493bfb5de69fbd
Jake Meredith <36305585+jrmeredith@users.noreply.github.com>
2018-07-12 12:01:04 -0700

    Updated main.cpp to read from bin.out
    Changed code in main.cpp so it will read instructions from
    binary file.

--- stack-vm/main.cpp
+++ stack-vm/main.cpp
@@ -1,10 +1,24 @@
+#include <iostream>
+#include <fstream>
 #include "stack-vm.h"
 using namespace std;
 
-int main() {
+int main(int argc, char *argv[]) {
+
+	if (argc < 2) {
+		cout << "Usage: " << argv[0] << " <filename>" << endl;
+		return 0;
+	}
+
+	ifstream r(argv[1], ios::binary);
+	i32 i;
+	vector<i32> prog;
+
+	while (r.read((char*)&i, sizeof(i))) {
+		prog.push_back(i);
+	}
 
 	StackVM vm;
-	vector<i32> prog{ 3, 4, 0x40000001, 5, 0x40000002, 3, 0x40000003, 2, 0x40000004, 0x40000000 };
 	vm.loadProgram(prog);
 	vm.run();
 
